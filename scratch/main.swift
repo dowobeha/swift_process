5b971e06925ea5a3c4838c39d4a40d63b151b70f
Lane Schwartz <dowobeha@gmail.com>
2016-11-16 15:08:48 -0600

    Initial Commit

---
+++ scratch/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  scratch
+//
+//  Created by Lane Schwartz on 2016-11-16.
+//  Copyright © 2016 Lane Schwartz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
